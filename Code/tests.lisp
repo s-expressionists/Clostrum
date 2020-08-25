2289a766c8a97f3e7795ede807f45a45e21e7f15
Daniel Kochmański <daniel@turtleware.eu>
2020-08-25 18:08:31 +0200

    tests: add tests for undefined function and cell eq-ity

--- Code/tests.lisp
+++ Code/tests.lisp
@@ -25,15 +25,17 @@
 ;;; are assumed to be merged (and that is permissible by the spec). Otherwise
 ;;; a constellation of three environments is created.
 ;;;
-;;; The fixture is an anaphoric macro introducing variables ^v, ^r, ^e and ^c.
+;;; The fixture is an anaphoric macro introducing variables cli, renv, eenv
+;;; and cenv which represent the client, run-time environment, evaluation
+;;; environment and compilation environment.
 (5am:def-fixture with-envs ()
-  (let* ((^v (make-instance *client-class*))
-         (^r (make-instance *run-time-environment-class*))
-         (^e (if (not *evaluation-environment-class*)
-                 ^r
-                 (make-instance *evaluation-environment-class* :parent ^r)))
-         (^c (make-instance *compilation-environment-class* :parent ^e)))
-    (declare (ignorable ^v ^r ^e ^c))
+  (let* ((cli (make-instance *client-class*))
+         (renv (make-instance *run-time-environment-class*))
+         (eenv (if (not *evaluation-environment-class*)
+                 renv
+                 (make-instance *evaluation-environment-class* :parent renv)))
+         (cenv (make-instance *compilation-environment-class* :parent eenv)))
+    (declare (ignorable cli renv eenv cenv))
     (&body)))
 
 ;;; Smoke tests
@@ -46,5 +48,42 @@
 
 ;;; run-time-environment tests
 
-(5am:test (crud-function :fixture with-envs)
-  (5am:is (null (env:fboundp ^v ^r 'function-1))))
+(5am:test (function-undefined :fixture with-envs)
+  ;; Symbol without a function definition.
+  (5am:is (null (env:fboundp                   cli renv 'unknown)))
+  (5am:is (null (env:special-operator          cli renv 'unknown)))
+  (5am:is (null (env:macro-function            cli renv 'unknown)))
+  (5am:is (null (env:compiler-macro-function   cli renv 'unknown)))
+  (5am:finishes (env:fmakunbound               cli renv 'unknown))
+  (multiple-value-bind (fun type)
+      (5am:finishes (env:fdefinition cli renv 'unknown))
+    (5am:is (eq type 'cl:undefined-function))
+    (5am:signals undefined-function (funcall fun))
+    ;; Test whether the function which signals undefined-function may accept
+    ;; arguments.
+    (5am:signals undefined-function (funcall fun :lambda 30 :answer 42)))
+  ;; Proclamations
+  (5am:is (null (env:function-type cli renv 'unknown)))
+  (5am:is (null (env:function-inline cli renv 'unknown)))
+  (let ((cell-1 (5am:finishes (env:function-cell cli renv 'unknown)))
+        (cell-2 (5am:finishes (env:function-cell cli renv 'unknown)))
+        (undefn (5am:finishes (env:function-unbound cli renv 'unknown))))
+    (5am:is (eq cell-1 cell-2))
+    (5am:is (eq undefn (car cell-1))))
+  (5am:is (null (env:function-description cli renv 'unknown))))
+
+(5am:test (function-cell-eq :fixture with-envs)
+  "Tests whether cells are EQ after the function is redefined."
+  (let ((cell-1 (env:function-cell cli renv 'fun))
+        (cell-2 (progn
+                  (setf (env:fdefinition cli renv 'fun) #'(lambda () 42))
+                  (env:function-cell cli renv 'fun)))
+        (cell-3 (progn
+                  (setf (env:fdefinition cli renv 'fun) #'(lambda () 30))
+                  (env:function-cell cli renv 'fun)))
+        (cell-4 (progn
+                  (env:fmakunbound cli renv 'fun)
+                  (env:function-cell cli renv 'fun))))
+    (5am:is (eq cell-1 cell-2))
+    (5am:is (eq cell-2 cell-3))
+    (5am:is (eq cell-3 cell-4))))
