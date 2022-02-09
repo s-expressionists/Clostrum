82010fe687a15335b26dba7a4821048b3db621ad
Robert Strandh <robert.strandh@gmail.com>
2022-02-09 18:32:22 +0100

    Define condition type to replace existing simple condition.

--- Code/conditions.lisp
+++ Code/conditions.lisp
@@ -1,2 +1,5 @@
 (cl:in-package #:clostrum)
 
+(cl:define-condition attempt-to-define-special-operator-for-existing-function
+    (cl:error)
+  ((%function-name :initarg :function-name :reader function-name)))
