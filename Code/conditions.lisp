ba788c05f01df78656c197112155bdca7cac6b66
Robert Strandh <robert.strandh@gmail.com>
2022-02-09 18:36:38 +0100

    Add :REPORT option to new conditoin type.

--- Code/conditions.lisp
+++ Code/conditions.lisp
@@ -2,4 +2,9 @@
 
 (cl:define-condition attempt-to-define-special-operator-for-existing-function
     (cl:error)
-  ((%function-name :initarg :function-name :reader function-name)))
+  ((%function-name :initarg :function-name :reader function-name))
+  (:report (cl:lambda (condition stream)
+             (cl:format stream
+                        "Attempt to define ~s as a special operator,~@
+                         but it already names a function."
+                        (function-name condition)))))
