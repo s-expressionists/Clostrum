97d08b89563bcbd98ceb3dae74b11a6e53c5eafc
Robert Strandh <robert.strandh@gmail.com>
2022-02-11 16:39:24 +0100

    New condition type.

--- Code/conditions.lisp
+++ Code/conditions.lisp
@@ -8,3 +8,12 @@
                      "Attempt to define ~s as a special operator,~@
                       but it already names a function."
                      (function-name condition)))))
+
+(define-condition env:attempt-to-define-special-operator-for-existing-macro
+    (error)
+  ((%function-name :initarg :function-name :reader function-name))
+  (:report (lambda (condition stream)
+             (format stream
+                     "Attempt to define ~s as a special operator,~@
+                      but it already names a macro."
+                     (function-name condition)))))
