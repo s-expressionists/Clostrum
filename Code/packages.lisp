58e4b129a990993d877b8beaf1d8d8065d41b8a0
Robert Strandh <robert.strandh@gmail.com>
2022-02-23 14:08:04 +0100

    Exort name of new condition type.

--- Code/packages.lisp
+++ Code/packages.lisp
@@ -26,6 +26,7 @@
    #:attempt-to-define-special-operator-for-existing-function
    #:attempt-to-define-special-operator-for-existing-macro
    #:attempt-to-define-function-for-existing-special-operator
+   #:attempt-to-define-function-for-existing-macro
    #:attempt-to-set-function-type-of-special-operator
    #:attempt-to-set-function-type-of-macro
    #:attempt-to-declare-inline-a-non-existing-function
