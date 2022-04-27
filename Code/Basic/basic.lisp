c92fd624f281244ca4fa7f72c7da4ddbf36bf404
Robert Strandh <robert.strandh@gmail.com>
2022-04-27 09:32:15 +0200

    Remove error checks from (SETF FDEFINITION)

--- Code/Basic/basic.lisp
+++ Code/Basic/basic.lisp
@@ -258,19 +258,6 @@
      (env run-time-environment)
      function-name)
   (let ((entry (get-function-entry function-name env)))
-    ;; Check for error situations.  We consider it an error to call
-    ;; this function, whether with NEW-VALUE being NIL or not, if
-    ;; there is an existing definition of the name either as a special
-    ;; operator or as a macro.
-    (unless (null entry)
-      (cond ((special-operator entry)
-             (error 'env:attempt-to-define-function-for-existing-special-operator
-                    :function-name function-name))
-            ((not (null (macro-function entry)))
-             (error 'env:attempt-to-define-function-for-existing-macro
-                    :function-name function-name))
-            (t
-             nil)))
     (if (null new-value)
         ;; Avoid creating a new entry if NEW-VALUE is NIL.
         (unless (null entry)
