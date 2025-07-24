a0e127320dd35bbe232882d0b39ddc9ebcd59ec3
Bike <aeshtaer@gmail.com>
2025-07-24 14:11:20 -0400

    Documentation fixes
    1) Having a page for clostrum-trucler doesn't actually make much
       sense, since all it does is define methods on existing GFs.
    2) The generated homepage on GitHub uses Staple's own logo for some
       reason, and also has seven links to itself with text "English".
       Not sure why but maybe this will fix it?

--- staple.ext.lisp
+++ staple.ext.lisp
@@ -1,9 +1,7 @@
 (staple:load-system-quietly :clostrum-basic)
-(staple:load-system-quietly :clostrum-trucler)
 
 (defmethod staple:subsystems ((system (eql (asdf:find-system :clostrum))))
-  (list (asdf:find-system :clostrum-basic)
-        (asdf:find-system :clostrum-trucler)))
+  (list (asdf:find-system :clostrum-basic)))
 
 (defmethod staple:packages ((system (eql (asdf:find-system :clostrum))))
   ;; the implementation package is excluded,
@@ -12,3 +10,12 @@
 
 (defmethod staple:packages ((system (eql (asdf:find-system :clostrum-basic))))
   (list (find-package "CLOSTRUM-BASIC")))
+
+(defmethod staple:documents ((system (eql (asdf:find-system :clostrum))))
+  ;; Shouldn't be necessary, but I'm trying to keep Staple from
+  ;; generating a bunch of extraneous "English" links.
+  (list (asdf:system-relative-pathname :clostrum "README.md")))
+
+(defmethod staple:images ((system (eql (asdf:find-system :clostrum))))
+  ;; None (try to avoid Staple putting in its own logo)
+  ())
